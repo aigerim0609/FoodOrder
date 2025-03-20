534051a80cbadc76cc1b266f9d45e781cda60c58
aigerim0609 <aigerim062009rasulovna@gmail.com>
2025-03-20 14:46:20 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,43 @@
+const menu_items = [ 
+    {
+        id:1,
+        title:'Hamburger',
+        price:175,
+        img:'./img/burger.png'
+    },
+    {
+        id:2,
+        title:'Soda',
+        price:65,
+        img:'./img/soda.png'
+    },
+    {
+        id:3,
+        title:'Tea',
+        price:25,
+        img:'./img/tea.png'
+    },
+    {
+        id:4,
+        title:'Sushi',
+        price:190,
+        img:'./img/sushi.png'
+    },
+    {
+        id:5,
+        title:'Coffee',
+        price:35,
+        img:'./img/coffee.png'
+    },
+    {
+        id:6,
+        title:'Pizza',
+        price:180,
+        img:'./img/pizza.png'
+    },
+    
+]
+
+const orders_basked = [
+    
+]
